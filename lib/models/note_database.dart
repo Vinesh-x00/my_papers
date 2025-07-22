c608a9ef341d5671a997004a9042118a97dabea8
vinesh <ubuntu@mail.fr>
2025-07-22 16:06:35 +0200

    CRUD functions are added into NoteDatabase class

--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:my_papers/models/note.dart';
+import 'package:my_papers/models/utils.dart';
 import 'package:my_papers/objectbox.g.dart'; // created by `flutter pub run build_runner build`
 
-class NoteDatabase {
+class NoteDatabase extends ChangeNotifier{
   late final Store _store;
   late final Box<Note> _noteBox;
 
@@ -24,8 +26,39 @@ class NoteDatabase {
 
   ///Add [Note] to db with encryption using [AESCipher]
   bool addNote(String text, DateTime date) {
+    final nt = _noteBox.query(Note_.date.equals(date.millisecondsSinceEpoch)).build().findFirst();
+    if(nt != null) {
+      return false;
+    }
     final note = Note(text: text, date: date, iv: "");
     _noteBox.put(note);
     return true;
   }
+
+  void fetchNote({DateTime? fdate}) {
+    fdate ??= DateTime.now();
+    final (start, end) = getWeekRange(fdate);
+    startDate = start;
+    endDate = end;
+    final query = _noteBox.query(Note_.date.between(start.millisecondsSinceEpoch, start.millisecondsSinceEpoch));
+    currentNotes.clear();
+    currentNotes.addAll(query.build().find());
+    notifyListeners();
+  }
+
+  void updateNote(int id, String text) {
+    final existingNote = _noteBox.get(id);
+    if(existingNote != null) {
+      existingNote.text = text;
+      _noteBox.put(existingNote);
+    }
+    fetchNote(fdate: startDate);
+
+  }
+
+  void deleteNote(int id) {
+    _noteBox.remove(id);
+    fetchNote(fdate: startDate);
+  }
+
 }
